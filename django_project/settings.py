37f7f004aa21c40648a5b8d9d48a15c09dd0e786
Sergio Rivera <sriveralopez50@gmail.com>
2020-12-08 10:28:42 +0000

    Server update

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,7 +38,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.sergiorivera.blog', 'sergiorivera.blog', '178.79.187.9', '127.0.0.1']
 
